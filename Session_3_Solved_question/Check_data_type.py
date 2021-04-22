a023e75eb2541f8dec2baf0e12675c588595723c
Kratos-28 <surajarya517@gmail.com>
2021-04-22 23:48:42 +0530

    updation

--- Session_3_Solved_question/Check_data_type.py
+++ Session_3_Solved_question/Check_data_type.py
@@ -1,14 +1,17 @@
-n1=int(input())
-n2=input()
-n3=float(input())
-
-x=int(input())
-y=int(input())
-
-z=complex(x,y)
-
-print(type(n1))
-print(type(n2))
-print(type(n3))
-print(type(z))
-
+number=input()
+if number.isalpha():
+    print("Type is String.")
+elif number=="0":
+    print("Type is Zero()0.")
+elif number.isnumeric():
+    print("Type is Integer or real number.")
+else:
+    try:
+        number=float(number)
+        print("Type is float number.")
+    except:
+        try:
+            number=complex(number)
+            print("Type is complex number.")
+        except:
+            print("Invalid type.")    
